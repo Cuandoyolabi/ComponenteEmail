a18c2e0e8314f81f9b5961567d439f5ccdd45f5c
Cuandoyolabi <adaminzunza3@gmail.com>
2024-03-28 11:34:52 -0600

    Ya termine el projecto, solo faltaba el codigo de JavaScript que verificaba si el correo es correcto.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const subscribe__btn = document.querySelector('.input__subscribe');
 const principal__container = document.querySelector('.container__principal__information');
 const container__thankYou = document.querySelector('.container__thanks__subscribing');
-
 const container__thankYou__span = document.querySelector('.container__thanks__subscribing__span');
 
 
-subscribe__btn.addEventListener('click', ()=>{
-    principal__container.classList.add('hidden');
-    container__thankYou.classList.add('showed');
-})
-
 function validateEmail(email){
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
+    const emailInput = document.querySelector('.input__email');
+    const emailError = document.querySelector('.bottons__spanError');
+        
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(emailInput.value === "" || emailInput.value != pattern){
+        emailError.textContent = "Valid email required";
+        emailInput.style.borderColor = "var(--Orange)";
+        emailInput.style.color = "var(--Orange)";
+        
+    } if (emailInput.value.match(pattern)){
+        principal__container.classList.add('hidden');
+        container__thankYou.classList.add('showed');
+    }
+};   
+               
+subscribe__btn.addEventListener('click', ()=>{
+    validateEmail();
+});
